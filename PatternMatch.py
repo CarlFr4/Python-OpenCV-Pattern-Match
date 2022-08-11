c3c0ab631a67b7e8a0931692972129bbbf6a7852
CarlFr4 <110987102+CarlFr4@users.noreply.github.com>
2022-08-11 12:33:04 -0500

    x, y coordinates and markers
    Code now finds x, y coordinates of the upper left corner of each part, and draws markers at those locations on the image

--- PatternMatch.py
+++ PatternMatch.py
@@ -33,17 +33,22 @@ yloc, xloc = np.where(myresult >= threshold)
 #draw a rectangle of the appropriate width and height at each of the x,y locations
 for (x, y) in zip(xloc, yloc):
     cv2.rectangle(imgrec, (x, y), (x + w, y + h), (0,255,255), 1)
-#finding All Contours
-imgrec = cv2.convertScaleAbs(imgrec, 3, 3)
-ret, imgrec = cv2.threshold(imgrec, 0, 255, cv2.THRESH_BINARY)
-imgrec = cv2.bitwise_not(imgrec)
-contours, hierarchy = cv2.findContours(imgrec, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE) # FIND ALL CONTOURS
-imgfinal2 = cv2.drawContours(imgfinal2, contours, -1, (0, 255, 0), 3) # DRAW ALL DETECTED CONTOURS
-#resize the images
+#find all the drawn rectangles using contours
+imgrec = cv2.convertScaleAbs(imgrec, 3, 3) #increase the image contrast again
+ret, imgrec = cv2.threshold(imgrec, 0, 255, cv2.THRESH_BINARY) #make the image binary
+imgrec = cv2.bitwise_not(imgrec) # reverse the image
+contours, hierarchy = cv2.findContours(imgrec, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE) #find contours on the image
+#imgfinal2 = cv2.drawContours(imgfinal2, contours, -1, (0, 255, 0), 3) #draw detected contours
+rectCon = myu.rectContour(contours) #find the rectangles
+for ct in range(0,len(rectCon)):
+    corners = myu.getCornerPoints(rectCon[ct]) #get corner points
+    coord = str(corners[0]).strip().strip('[').strip(']').split() #separate top left corner into x and y coordinates
+    imgfinal2 = cv2.drawMarker(imgfinal2, (int(np.float32(coord[0])), int(np.float32(coord[1]))), (0, 0, 255), 0, 100, 12, 8) #draw a marker at the top left
+    #print(coord[0] + '|' + coord[1]) #print the top left coordinate
+#resize and show the images
 img1 = cv2.resize(imgrotated, None, fx=1.1, fy=1.1)
 img2 = cv2.resize(img2, None, fx=0.2, fy=0.2)
 imgfinal2 = cv2.resize(imgfinal2, None, fx=0.2, fy=0.2)
-#show the images
 cv2.imshow('Modified Search Image', img1)
 cv2.imshow('Modified Camera Image', img2)
 cv2.imshow('Final Image', imgfinal2)
---
+++ myutils.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+def rectContour(contours):
+    rectCon = []
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if len(approx) == 4:
+                rectCon.append(i)
+    rectCon = sorted(rectCon, key=cv2.contourArea,reverse=True)
+    #print(len(rectCon))
+    return rectCon
+
+def getCornerPoints(cont):
+    peri = cv2.arcLength(cont, True) # LENGTH OF CONTOUR
+    approx = cv2.approxPolyDP(cont, 0.02 * peri, True) # APPROXIMATE THE POLY TO GET CORNER POINTS
+    return approx 
